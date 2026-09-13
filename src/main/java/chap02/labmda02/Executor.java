4566eebb15d26e458af2127986ac7ac385a202c0
Yuta Miyamoto <y.miyamoton@gmial.com>
2026-09-13 12:16:23 +0900

    [add] chapter02

---
+++ src/main/java/chap02/labmda02/Executor.java
@@ -0,0 +1,7 @@
+package chap02.labmda02;
+
+@FunctionalInterface
+public interface Executor {
+
+    void exec();
+}
---
+++ src/main/java/chap02/labmda02/ExecutorImpl.java
@@ -0,0 +1,9 @@
+package chap02.labmda02;
+
+public class ExecutorImpl implements Executor {
+
+    @Override
+    public void exec() {
+        System.out.println("クラスを使いました。");
+    }
+}
